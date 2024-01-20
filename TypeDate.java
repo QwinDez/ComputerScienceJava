875910d52bb116c21f441c64c9da107d6254d385
da_soldatkin <da_soldatkin@controlsystems.ru>
2024-01-20 16:03:25 +0300

    Second commit

--- TypeDate.java
+++ TypeDate.java
@@ -2,7 +2,7 @@ package ru.example.chapter001;
 
 public class TypeDate {
     public static void main(String[] args) {
-        int a = Integer.parseInt(args[0]), b = Integer.parseInt(args[1]);
+        int a = 15, b = 15;
         System.out.printf("a = %d, b = %d", a, b);
     }
 }
