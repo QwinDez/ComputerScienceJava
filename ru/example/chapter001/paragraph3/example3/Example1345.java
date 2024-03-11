d3533a673b857e599bbe2ad7fa67e8a258158018
da_soldatkin <da_soldatkin@controlsystems.ru>
2024-03-11 17:51:28 +0300

    Задачи из главы 1, раздела 3, упражнения 1.3.45

---
+++ ru/example/chapter001/paragraph3/example3/Example1345.java
@@ -0,0 +1,16 @@
+package ru.example.chapter001.ru.example.chapter001.paragraph3.example3;
+
+import java.util.Scanner;
+
+public class Example1345 {
+    public static void main(String[] args) {
+        double x = new Scanner(System.in).nextDouble();
+        double r = 1.0 - x;
+        int t = 10;
+        for (int i = 0; i < t; i++) {
+            x = 1.0 - 1.0 / r;
+            x += t;
+            System.out.println(x);
+        }
+    }
+}
