0b4059dab42245efe7c668756da3adf43a8a7fac
da_soldatkin <da_soldatkin@controlsystems.ru>
2024-03-14 18:25:33 +0300

    Задачи из главы 1, раздела 4(Sample, CouponCollector)

--- ru/example/chapter001/paragraph4/CouponCollector.java
+++ ru/example/chapter001/paragraph4/CouponCollector.java
@@ -7,6 +7,7 @@ public class CouponCollector {
         int n = new Scanner(System.in).nextInt();
         boolean[] isCollected = new boolean[n];
         int count = 0, distinct = 0;
+
         while (distinct < n) {
             int r = (int) (Math.random() * n);
             count++;
