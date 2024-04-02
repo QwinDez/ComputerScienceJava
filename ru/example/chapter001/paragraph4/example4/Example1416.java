82fb5ca9af0a69c958ee5336a079c2a2662433b2
Des1ty <dezzersold93@gmail.com>
2024-04-03 01:00:54 +0300

    Упражнение 1.4.16

--- ru/example/chapter001/paragraph4/example4/Example1416.java
+++ ru/example/chapter001/paragraph4/example4/Example1416.java
@@ -5,21 +5,37 @@ import java.util.Scanner;
 public class Example1416 {
     public static void main(String[] args) {
         int n = new Scanner(System.in).nextInt();
-        boolean[] isPrime = new boolean[n + 1];
+        boolean[][] a = new boolean[n + 1][n + 1];
         for (int i = 2; i <= n; i++)
-            isPrime[i] = true;
+            for (int j = 2; j <= n; j++) {
+                a[i][j] = true;
+            }
 
-        for (int i = 2; i <= n / i; i++) {
-            if (isPrime[i]) {
-                for (int j = i; j <= n / i; j++) {
-                    isPrime[i * j] = false;
+        for (int i = 2; i <= n; i++) {
+            for (int j = 2; j <= n; j++) {
+                if (a[i][j]) {
+                    if (!isPrime(i)) {
+                        a[i][j] = false;
+                    } else if (!isPrime(j)) {
+                        a[i][j] = false;
+                    }
                 }
             }
         }
 
-        for (boolean booleans : isPrime) {
-            System.out.print(booleans + " ");
+        for (boolean[] booleans : a) {
+            for (boolean aBoolean : booleans) {
+                System.out.print(aBoolean + " ");
+            }
             System.out.println();
         }
+    }
+    public static boolean isPrime (int n) {
+        if (n < 2)
+            return false;
+        for (int i = 2; i * i <= n; i++)
+            if (n % i == 0)
+                return false;
+        return true;
     }
 }
